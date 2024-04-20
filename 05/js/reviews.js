346b9767a241d1d9f6d105fbd8e08f89be1ba217
hannahvramos <hannahvictoriar.com>
2024-04-19 23:21:59 -0600

    first 05 commit

---
+++ 05/js/reviews.js
@@ -0,0 +1,27 @@
+export const reviews = [
+    {
+        "name": "Elena Drayton",
+        "stars": 5,
+        "text": "Exceptional service! From the moment I walked in, I felt valued and respected. The staff was knowledgeable, and they helped me find the perfect car within my budget. Highly recommend this dealership to anyone looking for a hassle-free experience.",
+    },
+    {
+        "name": "Blake Lloyd",
+        "stars": 4,
+        "text": "Great selection of vehicles and friendly staff. They were patient with all my questions and never pressured me into making a decision. The purchasing process was smooth, and I left feeling confident in my choice. Will definitely return for future purchases.",
+    },
+    {
+        "name": "Brenda Garcia",
+        "stars": 3,
+        "text": "Overall, a decent experience. The sales team was friendly, but I felt a bit rushed during the negotiation process. However, they did manage to find me a good deal on the car I wanted. Just wish there was a bit more transparency in pricing.",
+    },
+    {
+        "name": "Abbey Cummings",
+        "stars": 2,
+        "text": "Disappointing service. The salesperson was unprofessional and seemed more interested in making a sale than understanding my needs. Additionally, the condition of the car I purchased was not as described. Would not recommend this dealership.",
+    },
+    {
+        "name": "Liam Hartmann",
+        "stars": 5,
+        "text": "Outstanding customer service from start to finish! The team went above and beyond to ensure I was satisfied with my purchase. They even followed up after the sale to make sure everything was going smoothly. Highly impressed and grateful for their attention to detail.",
+    },
+]
---
+++ 05/js/script.js
@@ -0,0 +1,45 @@
+function toggleMenu() {
+    document.getElementById("primaryNav").classList.toggle("open");
+    document.getElementById("hamButton").classList.toggle("open");
+}
+const x = document.getElementById("hamButton");
+
+x.onclick = toggleMenu;
+
+// inject current year in folder
+const rightNow = new Date();
+document.querySelector('#year').textContent = rightNow.getFullYear();
+
+//reviews
+import { reviews } from '../js/reviews.js';
+console.log(reviews);
+
+//create card
+const myTarget = document.querySelector("#reviews");
+
+for (let x = 0; x < reviews.length; x++) {
+
+    //create section
+    const mySection = document.createElement('section')
+    const myName = document.createElement('h3')
+    myName.textContent = reviews[x].name
+
+    const myPara = document.createElement('p')
+    myPara.textContent = reviews[x].text
+
+    const myReview = document.createElement('div')
+
+    for (let step = 0; step<reviews[x].stars; step++) {
+        const myStar = document.createElement('img')
+        myStar.src = "../images/star.svg"
+        myReview.appendChild(myStar)
+    }
+
+    //build figure w child
+    mySection.appendChild(myName);
+    mySection.appendChild(myReview);
+    mySection.appendChild(myPara);
+
+    //new card
+    myTarget.appendChild(mySection);
+}
