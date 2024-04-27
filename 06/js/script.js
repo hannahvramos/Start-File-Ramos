99490b3da77abc2aba98af1e55e982fe06278f6c
hannahvramos <hannahvictoriar.com>
2024-04-27 08:56:35 -0600

    06 commit

---
+++ 06/js/script.js
@@ -0,0 +1,12 @@
+function toggleMenu() {
+    document.getElementById("primaryNav").classList.toggle("open");
+    document.getElementById("hamButton").classList.toggle("open");
+}
+const x = document.getElementById("hamButton");
+
+x.onclick = toggleMenu;
+
+// inject current year in folder
+const rightNow = new Date();
+document.querySelector('#year').textContent = rightNow.getFullYear();
+
